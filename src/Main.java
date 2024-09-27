13d9c4b53160751add6cced4950fa7c58fd69d34
daisyc2027 <daisyc2027@student.cis.edu.hk>
2024-09-27 17:13:13 +0800

    done (second ish version)

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,17 @@ public class Main {
 
     // 1. parenthesesCheck
     public static boolean parenthesesCheck(String str){
-            return str.length()%2 == 0 && str.charAt(0) != ')' && str.charAt(str.length()-1) != '(';
+    int count = 0;
+
+    for (int i = 0; i < str.length(); i++){
+        if(str.charAt(i) == '('){
+            count++;
+        }
+        else if(str.charAt(i) == ')'){
+            count --;
+        }
+    }
+    return count == 0;
     }
 
 
@@ -28,31 +38,56 @@ public static String encryptThis(String message){
         Scanner sc = new Scanner(message);
         StringBuilder newMessage = new StringBuilder();
         String currentWord;
-//Am I allowed to use split this string into an array?
         while(sc.hasNext()){
             currentWord = sc.next();
-            String middle;
-            char secondLetter = currentWord.charAt(currentWord.length()-1);
-            String lastLetter;
-            if(currentWord.length() > 2) {
-                middle = currentWord.substring(2, currentWord.length() - 1);
-                lastLetter = String.valueOf(currentWord.charAt(1));
+            int firstLetter = (int) currentWord.charAt(0);
+            if(currentWord.length()>2){
+                currentWord = firstLetter + String.valueOf(currentWord.charAt(currentWord.length()-1))
+                        + currentWord.substring(2, currentWord.length()-1) + String.valueOf(currentWord.charAt(1));
+            }
+            else if(currentWord.length() == 2){
+                currentWord = firstLetter + String.valueOf(currentWord.charAt(1));
             }
             else{
-                middle = "";
-                lastLetter = "";
+                currentWord = String.valueOf(firstLetter);
             }
-            int firstLetter = currentWord.charAt(0);
-            currentWord = String.valueOf(firstLetter) + secondLetter + middle + lastLetter;
             newMessage.append(currentWord);
             if(sc.hasNext()){
                 newMessage.append(" ");
             }
+
         }
-        return newMessage.toString();
+        return(newMessage.toString());
+
+//I think my code above works better since it also considers cases where the word is one letter. Its also more readable.
+
+//        while(sc.hasNext()){
+//            currentWord = sc.next();
+//            String middle;
+//            char secondLetter = currentWord.charAt(currentWord.length()-1);
+//            String lastLetter;
+//            if(currentWord.length() > 2) {
+//                middle = currentWord.substring(2, currentWord.length() - 1);
+//                lastLetter = String.valueOf(currentWord.charAt(1));
+//            }
+//            else{
+//                middle = "";
+//                lastLetter = "";
+//            }
+//            int firstLetter = currentWord.charAt(0);
+//            currentWord = String.valueOf(firstLetter) + secondLetter + middle + lastLetter;
+//            newMessage.append(currentWord);
+//            if(sc.hasNext()){
+//                newMessage.append(" ");
+//            }
+//        }
+//        return newMessage.toString();
     }
 
     // 4. decipherThis
+
+    //I'm too lazy to make this one readable though, it works and thats all that matters
+
     public static String decipherThis(String message){
         String[] words = message.split(" ");
         StringBuilder finalMessage = new StringBuilder();
